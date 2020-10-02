d6eabce8a0d6e14d4348bc30e71ea9e9a87780fe
grenkoca <grenkoca@gmail.com>
2020-10-02 14:34:23 -0400

    Updated mine_patches.py to support input/output csv options

--- mine_patches.py
+++ mine_patches.py
@@ -34,19 +34,26 @@ def generate_initial_mask(slide_path, show_overlay=False):
     # Remove pixels above G/B color difference threshold
     thumb_pen_mask = pen_mask(slide_thumbnail)
     hybrid_mask = np.logical_and(thumb_whitespace_mask, thumb_pen_mask)
-    # Get all values with low saturation (gray) or value (darkness), remove
+    
     hsv_mask = HSV_mask(slide_thumbnail)
-    final_mask = np.logical_and(hybrid_mask, hsv_mask)
+
+    final_mask = np.logical_and(hsv_mask, hybrid_mask)
     final_mask = binary_fill_holes(final_mask)
     final_mask = diate_and_erode(final_mask)
-    if show_overlay:
-        overlay = slide_thumbnail.copy()
-        overlay[~final_mask] = overlay[~final_mask] // 2
-        plt.imshow(overlay)
-        plt.show()
+
     return final_mask, real_scale
 
+def generate_initial_mask_binary(slide_path):
+    slide = openslide.open_slide(slide_path)
+    slide_dims = slide.dimensions
+
+    # Call thumbnail for effiency, calculate scale relative to whole slide
+    slide_thumbnail = np.asarray(slide.get_thumbnail((slide_dims[0]//SCALE, slide_dims[1]//SCALE)))
+    real_scale = (slide_dims[0]/slide_thumbnail.shape[1], slide_dims[1]/slide_thumbnail.shape[0])
+    return cv2.cvtColor(slide_thumbnail, cv2.COLOR_RGB2GRAY) > 0, real_scale
+
 if __name__ == '__main__':
+    start = time.time()
     parser = argparse.ArgumentParser()
 
     parser.add_argument('-i', '--input_path',
@@ -60,8 +67,14 @@ if __name__ == '__main__':
                         dest='annotation_mask_path',
                         help="input path for the label mask")
     parser.add_argument('-o', '--output_path',
-                        dest='output_path',
+                        dest='output_path', default=None,
                         help="output path for the patches")
+    parser.add_argument('-ocsv', '--output_csv',
+                        dest='output_csv', default=None,
+                        help="output path for the csv.")
+    parser.add_argument('-icsv', '--input_csv',
+                        dest='input_csv', default=None,
+                        help="CSV with x,y coordinates of patches to mine.")
     parser.add_argument('-lm', '--landmarks_path',
                         dest='landmarks_path',
                         help="output path for the coordinates"+\
@@ -69,17 +82,25 @@ if __name__ == '__main__':
     parser.add_argument('-t', '--threads',
                         dest='threads',
                         help="number of threads, by default will use all")
+    
     args = parser.parse_args()
+    if args.output_path is None:
+        do_save_patches = False
+        out_dir = ""
+    else: 
+        if not os.path.exists(args.output_path):
+            print("Output Directory does not exist, we are creating one for you.")
+            Path(args.output_path).mkdir(parents=True, exist_ok=True)
+        
+        do_save_patches = True
+        out_dir = os.path.abspath(args.output_path)
+        if not out_dir.endswith("/"):
+            out_dir += "/"
     # Path to openslide supported file (.svs, .tiff, etc.)
     slide_path = os.path.abspath(args.input_path)
 
     if not os.path.exists(slide_path):
         raise ValueError("Could not find the slide, could you recheck the path?")
-
-    if args.output_path is not None:
-        if not os.path.exists(args.output_path):
-            print("Output Directory does not exist, we are creating one for you.")
-            Path(args.output_path).mkdir(parents=True, exist_ok=True)
 
     if args.landmarks_path is not None:
         if not os.path.exists(args.landmarks_path):
@@ -87,27 +108,26 @@ if __name__ == '__main__':
             Path(args.output_path).mkdir(parents=True, exist_ok=True)
 
     if args.output_path is None and args.landmarks_path is None:
-        raise ValueError("Please atleast give output_path or landmarks_path")
+        print("No patch output path found-- not saving.")
 
     if args.tissue_mask_path is None:
         print("Tissue Mask is was not provided. We are creating one for you.")
-    if args.label_mask_path is None:
+    if args.annotation_mask_path is None:
         print("No label Mask was provided. Using Tissue Mask for extraction.")
-    out_dir = os.path.abspath(args.output_path)
-    if not out_dir.endswith("/"):
-        out_dir += "/"
     # Create new instance of slide manager
     manager = PatchManager(slide_path)
 
-    # Generate an initial validity mask
-    valid_mask, scale = generate_initial_mask(slide_path, SHOW_VALID)
-    manager.set_valid_mask(valid_mask, scale)
-    
-    # Create partial gaussian blur validity check.
-    # This allows us to create methods that only have one argument (the patch) for validity checking.
-    gaussian_blur_check = partial(gaussian_blur, upperlimit=UPPER_LIMIT, lowerlimit=LOWER_LIMIT)
-    manager.add_patch_criteria(gaussian_blur_check)
+    if args.input_csv is None:
+        # Generate an initial validity mask
+        mask, scale = generate_initial_mask(args.input_path)
+        print("Setting valid mask...")
+        plt.imshow(mask)
+        plt.show()
+        manager.set_valid_mask(mask, scale)
+        manager.set_openslide_mask = args.annotation_mask_path
+        # Save patches releases saves all patches stored in manager, dumps to specified output file
+        manager.save_patches(out_dir, n_patches=1000, output_csv=args.output_csv, n_jobs=NUM_WORKERS, save=do_save_patches)
+        print("Total time: {}".format(time.time() - start))
+    else:
+        manager.saved_defined_patches(out_dir, patch_coord_csv=args.input_csv)
 
-    # Save patches releases saves all patches stored in manager, dumps to specified output file
-    manager.save_patches(out_dir, n_patches=1000, n_jobs=NUM_WORKERS)
-    print("Total time: {}".format(time.time() - start))
