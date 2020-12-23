d2e6a9bf7d9440e3bb9aab73dbc30dff46468e61
Sarthak Pati <sarthak.pati@hotmail.com>
2020-12-23 14:11:18 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+  readme = readme_file.read()
 
 requirements = [
     'openslide-python',
@@ -10,13 +13,28 @@ requirements = [
 ]
 
 setup(
-    name='OPM-GANDLF',
+    name='OpenPatchMiner',
     version='0.1.0',
     packages=['opm'],
+     python_requires='>=3.6',
     install_requires=requirements,
-    url='https://github.com/grenkoca/OPM-GANDLF',
+    url='https://github.com/CBICA/OPM',
     license='BSD-3-Clause License',
     author='Caleb Grenko',
-    author_email='grenkoca@gmail.com',
-    description='A patch miner for GANDLF'
+    author_email='software@cbica.upenn.edu',
+    description='A patch miner for large histopathology images',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD-3-Clause License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    long_description=readme,
+    include_package_data=True,
+    keywords='histopathylogy, patch, miner',
+    zip_safe=False,
 )
