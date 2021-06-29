bcbb2d9b1239bccf37089341bd2d346e6576974a
Sarthak Pati <sarthak.pati@hotmail.com>
2021-06-29 08:32:17 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ requirements = [
 
 setup(
     name='OpenPatchMiner',
-    version='0.1.3',
+    version='0.1.4',
     python_requires='>=3.6',
     packages=find_packages(),
     install_requires=requirements,
